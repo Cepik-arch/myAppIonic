a4cb859cb7fc46bb32ce4c4b8f632de53b7e1b0f
Jiří Čepela <j_cepela@ad.utb.cz>
2021-11-04 15:13:58 +0100

    Signed-off-by: Jiří Čepela <j_cepela@ad.utb.cz>

--- myApp/src/app/tab3/tab3.page.ts
+++ myApp/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Storage } from '@capacitor/storage';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  keyfirstName;
+  firstname = "";
+
+  constructor() { 
+    this.getName();
+  }
+
+  private async getName(){
+
+    const {value} = await Storage.get({
+      key: this.keyfirstName
+    })
+
+    this.firstname = JSON.parse(value);
+  }
+
+  async onInput(event: any) {
+    console.log(event.target.value);
+
+    await Storage.set({
+      key: this.keyfirstName,
+      value: event.target.value,
+    });
+
+  }
 
 }
